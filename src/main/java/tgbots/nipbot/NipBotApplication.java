271b515efb8a23fe3d4e2ba4c09266db0e25c20c
Ivan <uvanes21@yandex.ru>
2022-07-17 19:15:11 +0300

    Initial commit

---
+++ src/main/java/tgbots/nipbot/NipBotApplication.java
@@ -0,0 +1,13 @@
+package tgbots.nipbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NipBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NipBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgbots/nipbot/NipBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tgbots.nipbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NipBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
