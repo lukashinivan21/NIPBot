95df8a1ea24e5934799a71ec7c8a31a997d90c22
pin20 <pin200048@gmail.com>
2022-07-22 22:32:32 +0400

    Создана база данных, в liquibase сделана тестовая таблица, настроена связь с телеграмм ботом

---
+++ src/main/java/tgbots/nipbot/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,42 @@
+package tgbots.nipbot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+            Long chatId = update.message().chat().id();
+            //если пришел любой меседж
+            if(update.message() != null){
+                SendMessage message = new SendMessage(chatId,"Связь налажена!");
+                telegramBot.execute(message);
+            }
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+}
